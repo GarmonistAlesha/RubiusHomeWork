0c00712b1415a8ae4f8bfe366bc9c9766015be74
Ponomarev Mikhail <mvponomarevwork@gmail.com>
2024-02-04 21:50:48 +0700

    Выполнено ДЗ№2
    Выполнено ДЗ №2

---
+++ Lession2/Lession2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.Design;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Определение високосного года");
+
+        var year = Console.ReadLine();//введение значения 
+
+        if (!uint.TryParse(year, out var value1)) // проверка на преобразование в положительное числовове значение
+        {
+            Console.WriteLine($"{year}- не валидное значение!!!");
+            return;
+        }
+        if (value1 > 30000) // проверка максимального вводимого значения года
+        {
+            Console.WriteLine("Неверное условие.P.S Год больше 30000");
+            return;
+        }
+        if (value1 % 4 == 0 || (value1 % 100 != 0 && value1 % 400 == 0))  // Условия високосного года 
+        {
+            Console.WriteLine("Yes");
+            return;
+           
+        }
+        if (value1 % 4 != 0 || (value1 % 100 == 0 && value1 % 400 != 0))   //Условия не соответствия високосному году                                                                           //
+        {
+            Console.WriteLine("No");
+            return;
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ответ 
